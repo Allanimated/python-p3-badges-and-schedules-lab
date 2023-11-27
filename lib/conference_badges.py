0979ec3b5285d3c4c6bf395bd2d6e17ea4652327
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-27 12:10:30 +0300

    Completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,17 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badge_messages_list = [f"Hello, my name is {name}." for name in names]
+    return badge_messages_list
 
 def assign_rooms(names):
-    return None
+    new_list = [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]
+    return new_list
 
 def printer(names):
-    return None
+    batch_badge_creator_list = batch_badge_creator(names)
+    assign_rooms_list = assign_rooms(names)
+    message_list = [*batch_badge_creator_list, *assign_rooms_list]
+    for message in message_list:
+        print(message)
